c0ff4dca69a17b7b29d572b44628bd468b9a37e5
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-21 22:27:50 +0200

    Removed useEffect for Fetching Ingredients in Ingredients Components because it is already done in the Search Component initially

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -9,23 +9,6 @@ const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
-    fetch('https://react-hooks-b09bb.firebaseio.com/ingredients.json')
-      .then(response => {
-        return response.json();
-      }).then(responseJson => {
-      const loadedIngredients = [];
-      Object.keys(responseJson).forEach(key => {
-        loadedIngredients.push({
-          id: key,
-          title: responseJson[key].title,
-          amount: responseJson[key].amount
-        });
-      });
-      setIngredients(loadedIngredients);
-    });
-  }, []);
-
-  useEffect(() => {
     console.log('Rendering ingredients', ingredients);
   }, [ingredients]);
 
