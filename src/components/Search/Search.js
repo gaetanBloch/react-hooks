ad3868a8b74fb647f16a976f4abb32907c895b6f
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-21 21:55:08 +0200

    Set and update Filter in the Search Component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+
+  const [filter, setFilter] = useState('');
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={filter}
+            onChange={event => setFilter(event.target.value)} />
         </div>
       </Card>
     </section>
