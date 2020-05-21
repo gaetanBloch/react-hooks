fcf36505c34a1f82a31f5e570ff8b17f91b7c3c7
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-21 21:57:55 +0200

    Fetch Ingredients every time the filter changed

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import './Search.css';
@@ -6,6 +6,22 @@ import './Search.css';
 const Search = React.memo(props => {
 
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    fetch('https://react-hooks-b09bb.firebaseio.com/ingredients.json')
+      .then(response => {
+        return response.json();
+      }).then(responseJson => {
+      const loadedIngredients = [];
+      Object.keys(responseJson).forEach(key => {
+        loadedIngredients.push({
+          id: key,
+          title: responseJson[key].title,
+          amount: responseJson[key].amount
+        });
+      });
+    });
+  }, [filter])
 
   return (
     <section className="search">
