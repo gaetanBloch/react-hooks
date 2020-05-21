1415835ab18504125fd026893f8cf96b648f46f4
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-21 23:04:32 +0200

    Added Error Handler when removing ingredient and display a modal

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,11 +3,13 @@ import React, { useCallback, useEffect, useState } from 'react';
 import IngredientForm from './IngredientForms/IngredientForm';
 import Search from '../Search/Search';
 import IngredientList from './IngredientList/IngredientList';
+import ErrorModal from '../UI/ErrorModal/ErrorModal';
 
 const Ingredients = () => {
 
   const [ingredients, setIngredients] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
 
   const filerIngredientsHandler = useCallback(filteredIngredients => {
     setIngredients(filteredIngredients);
@@ -36,18 +38,27 @@ const Ingredients = () => {
   const removeIngredientHandler = id => {
     setLoading(true);
     fetch(
-      `https://react-hooks-b09bb.firebaseio.com/ingredients/${id}.json`,
+      `https://react-hooks-b09bb.firebaseio.com/ingredients/${id}.jon`,
       { method: 'DELETE' }
     ).then(() => {
       setLoading(false);
       setIngredients(preIngredients => preIngredients.filter(ingredient => {
         return ingredient.id !== id;
       }));
+    }).catch(error => {
+      setError(error.message);
     });
   };
 
+  const clearErrorHandler = () => {
+    setError(null);
+  }
+
   return (
     <div className="App">
+
+      {error && <ErrorModal onClose={clearErrorHandler}>{error}</ErrorModal>}
+
       <IngredientForm
         onAddIngredient={addIngredientHandler}
         loading={loading} />
