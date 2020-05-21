166629582ced70269fe432e84375daf2b6a1424a
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-22 00:49:19 +0200

    Used useContext Hook to set-up mock authentication page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AuthContextProvider from './context/auth-context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
