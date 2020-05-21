a382814b276d7e15ce34260e8f199c9d5eb0744f
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-21 15:37:15 +0200

    Using useState in IngredientForm Component

--- src/components/Ingredients/IngredientForms/IngredientForm.js
+++ src/components/Ingredients/IngredientForms/IngredientForm.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../../UI/Card/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+
+  const state = useState({
+    title: '',
+    amount: '',
+  });
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +21,20 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={state[0].title}
+              onChange={event => state[1]({ title: event.target.value })} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={state[0].amount}
+              onChange={event => state[1]({ amount: event.target.value })}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
